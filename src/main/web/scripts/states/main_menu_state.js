ef3721c057b434194e3ca1fe95a63d4167d7c7ed
Ray Toal <rtoal@lmu.edu>
2010-09-15 16:09:47 +0000

    Add an incredibly annoying transformation just to illustrate that the animation framework is operational

--- src/main/web/scripts/states/main_menu_state.js
+++ src/main/web/scripts/states/main_menu_state.js
@@ -1,25 +1,32 @@
 /**
  * main_menu_state.js
- * 
+ *
  * This is the initial state of the game. It displays a menu for the user.
- * 
+ *
  */
 
 $(document).ready(function() {
-	m3.game.states.MainMenuState = function() {};
-	
-	m3.game.states.MainMenuState.prototype.update = function() {
-		// Basic example of drawing a shape.
-		m3.game.context.fillStyle = "rgb(200, 220, 250)";
-		m3.game.context.fillRect(0, 0, m3.config.width, m3.config.height);
-		
-		// Example of drawing text.
-		m3.game.context.fillStyle   = "rgba(0, 0, 0, 0.8)";
-		m3.game.context.font        = "bold 48px sans-serif";
-		m3.game.context.textAlign   = "center";
-		m3.game.context.fillText("Manic Monkey Madness!!!", m3.config.width / 2, m3.config.height / 2);
-		
-		// This function needs to instead draw the actual main menu, and
-		// also check for user input and react accordingly.
-	};
+    m3.game.states.MainMenuState = function() {};
+
+    m3.game.states.MainMenuState.prototype.update = function () {
+        var context = m3.game.context,
+            halfWidth = m3.config.width / 2,
+            halfHeight = m3.config.height / 2;
+
+        // Basic example of drawing a shape.
+        context.fillStyle = "rgb(200, 220, 250)";
+        context.fillRect(0, 0, m3.config.width, m3.config.height);
+
+        // Example of drawing text.
+        context.fillStyle   = "rgba(0, 0, 0, 0.8)";
+        context.font        = "bold 48px sans-serif";
+        context.textAlign   = "center";
+        context.translate(halfWidth, halfHeight);
+        context.rotate(.02);
+        context.translate(-halfWidth, -halfHeight);
+        context.fillText("Manic Monkey Madness!!!", halfWidth, halfHeight);
+
+        // This function needs to instead draw the actual main menu, and
+        // also check for user input and react accordingly.
+    };
 });
