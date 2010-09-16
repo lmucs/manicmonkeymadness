84dd1c534ea505cd47f2ee8574f53666fbc20f2f
Jarod Long <jlong22@me.com>
2010-09-16 21:34:55 +0000

    Created an input module that handles keyboard input, and soon will handle mouse input as well.
    The player can now transition from the main menu state to the play state by pressing enter.

--- src/main/web/scripts/setup.js
+++ src/main/web/scripts/setup.js
@@ -9,27 +9,30 @@
 var m3 = {};
 
 $(function() {
-	/**
-	 * The game object contains common objects such as the canvas.
-	 */
-	m3.game = function() {
-		return {
-			// Canvas element and context.
-			canvas:  document.getElementById("game_canvas"),
-			context: document.getElementById("game_canvas").getContext("2d"),
-			
-			// The time elapsed since the last frame.
-			elapsed: 0,
-			
-			// Contains the constructors for all the states in the game.
-			states: {},
-		};
-	}();
-	
-	/**
-	 * Initialize the game.
-	 */
-	m3.game.init = function() {
-		m3.game.state = new m3.game.states.PlayState();
-	};
+    /**
+     * The game object contains common objects such as the canvas.
+     */
+    m3.game = function() {
+        return {
+            // Canvas element and context.
+            canvas:  document.getElementById("game_canvas"),
+            context: document.getElementById("game_canvas").getContext("2d"),
+            
+            // The time elapsed since the last frame.
+            elapsed: 0,
+            
+            // Contains the constructors for all the states in the game.
+            states: {},
+        };
+    }();
+    
+    /**
+     * Initialize the game.
+     */
+    m3.game.init = function() {
+        m3.game.state = new m3.game.states.MainMenuState();
+        
+        document.onkeydown = m3.input.processKeyDown;
+        document.onkeyup   = m3.input.processKeyUp;
+    };
 });
--- src/main/web/scripts/states/play_state.js
+++ src/main/web/scripts/states/play_state.js
@@ -7,39 +7,49 @@
  */
 
 $(function() {
-	
-	var x = 150;
-	var y = 150;
-	var dx = 2;
-	var dy = 4;
-	
-	m3.game.states.PlayState = function() {};
-	
-	m3.game.states.PlayState.prototype.update = function() {
-		var canvas = document.getElementById("game_canvas"); 
-		m3.launcher.launch(canvas);
-		
-		//define the ball
-	    var projectile = m3.game.context,
+    var x = 150;
+    var y = 150;
+    var dx = 2;
+    var dy = 4;
+    
+    m3.game.states.PlayState = function() {};
+    
+    m3.game.states.PlayState.prototype.keyHandlers = {
+        A: {
+            down: function() {
+                m3.util.log("Pressed A!");
+            },
+            
+            up: function() {
+                m3.util.log("Released A!");
+            },
+        },
+    };
+    
+    m3.game.states.PlayState.prototype.update = function() {
+        var canvas = document.getElementById("game_canvas"); 
+        m3.launcher.launch(canvas);
+        
+        //define the ball
+        var projectile = m3.game.context,
         halfWidth = m3.config.width / 2,
         halfHeight = m3.config.height / 2;
-		 
-		//draw a circle
-		projectile.beginPath();
-		projectile.arc(x, y, 10, 0, Math.PI*2, true);
-		projectile.closePath();
-		projectile.fill();
-		
-		//make it bounce
-		if (x + dx > m3.config.width || x + dx < 0)
-		    dx = -dx;
-	    if (y + dy > m3.config.height || y + dy < 0)
-			dy = -dy;
-		
-		//update location
-		x += dx;
-		y += dy;
-		
-	};
-	
+        
+        //draw a circle
+        projectile.beginPath();
+        projectile.arc(x, y, 10, 0, Math.PI*2, true);
+        projectile.closePath();
+        projectile.fill();
+        
+        //make it bounce
+        if (x + dx > m3.config.width || x + dx < 0)
+            dx = -dx;
+        if (y + dy > m3.config.height || y + dy < 0)
+            dy = -dy;
+        
+        //update location
+        x += dx;
+        y += dy;
+        
+    };
 });
