c7266cd1b112e8b94c42302a58748ad33520a33e
jarod.long64 <jarod.long64@19108cbd-a829-df12-ab1d-8371e1d2e174>
2010-09-16 21:34:28 +0000

    Created an input module that handles keyboard input, and soon will handle mouse input as well.
    The player can now transition from the main menu state to the play state by pressing enter.

--- trunk/src/main/web/scripts/states/main_menu_state.js
+++ trunk/src/main/web/scripts/states/main_menu_state.js
@@ -7,28 +7,43 @@
 
 $(function() {
     m3.game.states.MainMenuState = function() {};
-
+    
+    m3.game.states.MainMenuState.prototype.keyHandlers = {
+        ENTER: {
+            down: function() {
+                m3.game.state = new m3.game.states.PlayState();
+            },
+        },
+        
+        SPACE: {
+            down: function() {
+                m3.util.log("Pressed space!");
+            },
+            
+            up: function() {
+                m3.util.log("Released space!");
+            },
+        },
+    };
+    
     m3.game.states.MainMenuState.prototype.update = function () {
-		var canvas = document.getElementById("game_canvas"); 
-		m3.launcher.launch(canvas);
+        m3.launcher.launch(m3.game.canvas);
+        
         var context = m3.game.context,
             halfWidth = m3.config.width / 2,
             halfHeight = m3.config.height / 2;
-
-        // Basic example of drawing a shape.
+        
+        // Draw the background.
         context.fillStyle = "rgb(200, 220, 250)";
         context.fillRect(0, 0, m3.config.width, m3.config.height);
-
-        // Example of drawing text.
-        context.fillStyle   = "rgba(0, 0, 0, 0.8)";
-        context.font        = "bold 48px sans-serif";
-        context.textAlign   = "center";
-        context.translate(halfWidth, halfHeight);
-        context.rotate(.02);
-        context.translate(-halfWidth, -halfHeight);
+        
+        // Draw the text.
+        context.fillStyle = "rgba(0, 0, 0, 0.8)";
+        context.font      = "bold 48px sans-serif";
+        context.textAlign = "center";
         context.fillText("Manic Monkey Madness!!!", halfWidth, halfHeight);
-
-        // This function needs to instead draw the actual main menu, and
-        // also check for user input and react accordingly.
+        
+        context.font = "bold 30px sans-serif";
+        context.fillText("Press enter to continue!", halfWidth, halfHeight + 80);
     };
 });
