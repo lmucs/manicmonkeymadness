b623105a311f2f820a9c26774ef93338df4437d4
Anthony Khayat <akhayat@lmu.edu>
2010-09-16 18:19:10 +0000

    put the code to launch in it's own module launcher.js

---
+++ src/main/web/scripts/modules/launcher.js
@@ -0,0 +1,35 @@
+		/**
+		 * launcher.js 
+		 * 
+		 * This module is supposed to determine a launch angle using click and drag.  The original
+		 * click location is supposed to be origin and the angle is determined by where the user 
+		 * lets go.   Similar to crush the castle
+		 */
+
+
+$(document).ready(function() {
+	m3.launcher = function() {
+		var mousePosition = {down: false};
+		return {
+			launch: function(canvas) {
+	            canvas.onmousedown = function(event) {
+		            //saves the coordinates where the mouse is first pressed down
+	                mousePosition.oldX = event.pageX;
+	                mousePosition.oldY = event.pageY;
+	                mousePosition.down = !mousePosition.down;
+	             }
+	             canvas.onmouseup = function(event) {
+	                 mousePosition.down = !mousePosition.down;
+	                 mousePosition.x = event.pageX;
+	                 mousePosition.y = event.pageY; 
+    	             /* Calculates the angle using the point where the mouse was first clicked
+	                  * and the place where it is let up.  Good ole trig.
+	                  */
+	                 var angle = Math.atan(-1 * (mousePosition.y - mousePosition.oldY) / (mousePosition.x - mousePosition.oldX)) * (180 / Math.PI);
+	                 m3.util.log("fire!!!  Angle = " + angle);
+	  
+	             }
+			},
+		};
+	}();
+});
