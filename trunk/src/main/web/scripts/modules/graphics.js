fb4a61c05f0234c82aee1055e27be30c152284ee
mkronk3 <mkronk3@19108cbd-a829-df12-ab1d-8371e1d2e174>
2010-09-21 07:13:37 +0000

    partial box2d demo

---
+++ trunk/src/main/web/scripts/modules/graphics.js
@@ -0,0 +1,69 @@
+/**
+ * graphics.js
+ * 
+ * This module stores the canvas drawing procedures for m3 shapes.
+ *  
+ */
+$(function() {
+	m3.graphics = function() {
+		
+		var drawCircle = function(shape, context) {
+			context.strokeStyle = '#EE3333';
+			context.beginPath();
+		   	var circle = shape;
+			var pos = circle.m_position;
+			var r = circle.m_radius;
+			var segments = 16.0;
+			var theta = 0.0;
+			var dtheta = 2.0 * Math.PI / segments;
+			// draw circle
+			context.moveTo(pos.x + r, pos.y);
+			for (var i = 0; i < segments; i++) {
+				var d = new b2Vec2(r * Math.cos(theta), r * Math.sin(theta));
+				var v = b2Math.AddVV(pos, d);
+				context.lineTo(v.x, v.y);
+				theta += dtheta;
+			}
+			context.lineTo(pos.x + r, pos.y);
+		
+			// draw radius
+			context.moveTo(pos.x, pos.y);
+			var ax = circle.m_R.col1;
+			var pos2 = new b2Vec2(pos.x + r * ax.x, pos.y + r * ax.y);
+			context.lineTo(pos2.x, pos2.y);
+			context.stroke();
+		};
+		
+		var drawPoly = function(shape, context) {
+			context.strokeStyle = '#EE3333';
+			context.beginPath();
+			var poly = shape;
+			var tV = b2Math.AddVV(poly.m_position, b2Math.b2MulMV(poly.m_R, poly.m_vertices[0]));
+			context.moveTo(tV.x, tV.y);
+			for (var i = 0; i < poly.m_vertexCount; i++) {
+				var v = b2Math.AddVV(poly.m_position, b2Math.b2MulMV(poly.m_R, poly.m_vertices[i]));
+				context.lineTo(v.x, v.y);
+			}
+			context.lineTo(tV.x, tV.y);
+			context.stroke();
+		};		
+		
+		return {					
+			drawCircle: drawCircle,
+			
+			drawPoly: drawPoly,
+			
+			drawWorld: function(world, context) {
+				for (var b = world.m_bodyList; b; b = b.m_next) {
+					for (var s = b.GetShapeList(); s != null; s = s.GetNext()) {
+						if (s.m_type == b2Shape.e_circleShape) {
+							drawCircle(s, context);
+						} else {
+							drawPoly(s, context);
+						}
+					}
+				}    
+			}
+		};
+	}();
+});	
--- trunk/src/main/web/scripts/modules/world.js
+++ trunk/src/main/web/scripts/modules/world.js
@@ -8,79 +8,55 @@
 
 $(function() {
 	m3.world = function() {
+		//create the world
 		var worldAABB = new b2AABB();
 		worldAABB.minVertex.Set(0, 0);
-		worldAABB.maxVertex.Set(m3.config.level_width, m3.config.level_height);
-		
-		var gravity = new b2Vec2(0, 300);
+		worldAABB.maxVertex.Set(m3.config.level_width, m3.config.level_height);		
+		var gravity = new b2Vec2(0, m3.config.gravity);
 		var doSleep = true;
-		
 		var world = new b2World(worldAABB, gravity, doSleep);
-		
-		var circleSd = new b2CircleDef();
-		circleSd.density = 1.0;
-		circleSd.radius = 2;
-		circleSd.restitution = 1.0;
-		circleSd.friction = 0;
-		var circleBd = new b2BodyDef();
-		circleBd.AddShape(circleSd);
-		circleBd.position.Set(150, 150);
-		world.CreateBody(circleBd);
+
+		//create the ground
+		var groundBox = new b2BoxDef();
+		groundBox.extents.Set(450, 20);
+		groundBox.restitution = 0.2;		
+		var groundBody = new b2BodyDef();
+		groundBody.AddShape(groundBox);
+		groundBody.position.Set(450, 440);
+		world.CreateBody(groundBody);				
+
+		//create some bodies
+        createBox(400, 345, 10, 80);
+        createBox(500, 345, 10, 80);
+        createBox(450, 255, 80, 10);       
+        createBall(450, 0, 10);
 			
-		function drawShape(shape, context) {
-			context.strokeStyle = '#EE3333';
-			context.beginPath();
-			switch (shape.m_type) {
-			  case b2Shape.e_circleShape:
-			   	var circle = shape;
-				var pos = circle.m_position;
-				var r = circle.m_radius;
-				var segments = 16.0;
-				var theta = 0.0;
-				var dtheta = 2.0 * Math.PI / segments;
-				// draw circle
-				context.moveTo(pos.x + r, pos.y);
-				for (var i = 0; i < segments; i++) {
-					var d = new b2Vec2(r * Math.cos(theta), r * Math.sin(theta));
-					var v = b2Math.AddVV(pos, d);
-					context.lineTo(v.x, v.y);
-					theta += dtheta;
-				}
-				context.lineTo(pos.x + r, pos.y);
-			
-				// draw radius
-				context.moveTo(pos.x, pos.y);
-				var ax = circle.m_R.col1;
-				var pos2 = new b2Vec2(pos.x + r * ax.x, pos.y + r * ax.y);
-				context.lineTo(pos2.x, pos2.y);
-				break;
-				
-			  case b2Shape.e_polyShape:
-				var poly = shape;
-				var tV = b2Math.AddVV(poly.m_position, b2Math.b2MulMV(poly.m_R, poly.m_vertices[0]));
-				context.moveTo(tV.x, tV.y);
-				for (var i = 0; i < poly.m_vertexCount; i++) {
-					var v = b2Math.AddVV(poly.m_position, b2Math.b2MulMV(poly.m_R, poly.m_vertices[i]));
-					context.lineTo(v.x, v.y);
-				}
-				context.lineTo(tV.x, tV.y);
-				break;
-			}
-			
-			context.stroke();
+		function createBox(x, y, width, height) {			
+			var boxSd = new b2BoxDef();
+			boxSd.extents.Set(width, height);
+			var boxBd = new b2BodyDef();
+			boxBd.density = 10.0;
+			//boxBd.restitution = 0.2;
+			boxBd.AddShape(boxSd);
+			boxBd.position.Set(x, y);
+			return world.CreateBody(boxBd);			
 		};
 		
-		function drawWorld(world, context) {
-			for (var b = world.m_bodyList; b; b = b.m_next) {
-				for (var s = b.GetShapeList(); s != null; s = s.GetNext()) {
-					drawShape(s, context);
-				}
-			}    
+		function createBall(x, y, rad, fixed) {
+			var ballSd = new b2CircleDef();
+			if (!fixed) ballSd.density = 1.0;
+			ballSd.radius = rad || 10;
+			ballSd.restitution = 0.2;
+			var ballBd = new b2BodyDef();
+			ballBd.AddShape(ballSd);
+			ballBd.position.Set(x,y);
+			return world.CreateBody(ballBd);
 		};
+
 		return {			
 			update: function() {
-				world.Step(m3.config.fps / 60.0, 1);
-				drawWorld(world, m3.game.context);
+				world.Step(60 / 60.0, 1);
+				m3.graphics.drawWorld(world, m3.game.context);
 			}		
 		};
 	}();
