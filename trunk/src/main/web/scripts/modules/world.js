bb4355e6a6e16eaa688419032b0524cca864aedd
mkronk3 <mkronk3@19108cbd-a829-df12-ab1d-8371e1d2e174>
2010-09-28 21:48:19 +0000

    fixed gravity and world step function rate calculation

--- trunk/src/main/web/scripts/modules/world.js
+++ trunk/src/main/web/scripts/modules/world.js
@@ -16,44 +16,48 @@ $(function() {
     	worldAABB.upperBound.Set(10000.0, 10000.0);
         //worldAABB.lowerBound.Set(-m3.config.level_width / m3.config.scaling_factor, -m3.config.level_height / m3.config.scaling_factor);
         //worldAABB.upperBound.Set(m3.config.level_width / m3.config.scaling_factor, m3.config.level_height / m3.config.scaling_factor);
-    	var gravity = new b2Vec2(0.0, 2);
+    	var gravity = new b2Vec2(0.0, 9.8);
     	var world = new b2World(worldAABB, gravity, true);
     	window.world = world;
 
     	// create the ground
     	var groundBodyDef = new b2BodyDef();
-    	groundBodyDef.position.Set((m3.config.level_width / 2) / m3.config.scaling_factor, 21.7);
+    	groundBodyDef.position.Set((m3.config.level_width / 2) / m3.config.scaling_factor, 21.6);
     	var groundBody = world.CreateBody(groundBodyDef);
     	var groundShapeDef = new b2PolygonDef();
     	groundShapeDef.restitution = 0.2;
-    	groundShapeDef.friction = 0.5;
+    	groundShapeDef.friction = 0.9;
     	groundShapeDef.density = 1.0;
     	groundBody.w = (m3.config.level_width / 2) / m3.config.scaling_factor;
-    	groundBody.h = 1.0;
+    	groundBody.h = 0.5;
     	groundShapeDef.SetAsBox(groundBody.w, groundBody.h);
     	groundBody.CreateShape(groundShapeDef);
     	groundBody.SynchronizeShapes();
 
     	// reference of the world's bodies
     	var bodies = [groundBody];
-
+    	
         //create walls
-        var wall1 = createBox(0.2, 6, 0.1, 15, true);
-        var wall2 = createBox(25, 6, 0.1, 15, true);
+        var wall1 = createBox(0.2, (m3.config.level_height / 2) / m3.config.scaling_factor, 0.1, 15, true);
+        var wall2 = createBox(m3.config.level_width / m3.config.scaling_factor - 0.2, (m3.config.level_height / 2) / m3.config.scaling_factor, 0.1, 15, true);
         
         //create some bodies  	
-    	createBox(10, 1, 3, 1, false, 1);
-    	createBox(13, 1, 1, 4, false, 1);
-    	createBox(15, 3, 1, 1, false, 1);
+    	createBox(10, 1, 1, 0.5, false, 1);
+    	createBox(13, 1, 1, 1, false, 1);
+    	createBox(15, 3, 0.5, 1, false, 1);
 
-//       var ball1 = createBall(5, 1, 0.5);
+    	createBox(9, 20, 1, 3, false, 1);
+    	createBox(12, 20, 1, 3, false, 1);
+    	createBox(14, 15, 3, 0.1, false, 1);
+    	
+        //var ball1 = createBall(5, 1, 0.5);
 
-       function createBox(x, y, width, height, fixed, density, restitution, friction) {
+        function createBox(x, y, width, height, fixed, density, restitution, friction) {
             var bodyDef = new b2BodyDef();
             bodyDef.position.Set(x, y);
             var body = world.CreateBody(bodyDef);
             var shapeDef = new b2PolygonDef();
-            shapeDef.restitution = restitution || 0.5;
+            shapeDef.restitution = restitution || 0.4;
             shapeDef.density = density || 0.0;
             shapeDef.friction = friction || 0.9;
             body.w = width;
@@ -83,18 +87,16 @@ $(function() {
             
         return {
         	bodies: bodies,
+            createBox: createBox,
+            createBall: createBall,
             update: function() {
-                var context = m3.game.context;
-                
+                var context = m3.game.context;                
                 context.save();
                 context.scale(m3.config.scaling_factor, m3.config.scaling_factor);
-                context.lineWidth = 0.1;
-                world.Step(m3.config.fps / 60.0, 10);
+                world.Step(1 / m3.config.fps, 100);
                 m3.graphics.drawWorld(bodies, context);
                 context.restore();
-            },
-            createBox: createBox,
-            createBall: createBall
+            }
         };
     }();
 });
