900a7f43c8ea23f80eaf9c7948c9f6e5f02d4af3
mkronk3 <mkronk3@19108cbd-a829-df12-ab1d-8371e1d2e174>
2010-09-28 21:29:09 +0000

    updated demo for new physics engine still needs tweaking

--- trunk/src/main/web/scripts/modules/graphics.js
+++ trunk/src/main/web/scripts/modules/graphics.js
@@ -6,12 +6,11 @@
  */
 $(function() {
 	m3.graphics = function() {
-		
 		var drawBall = function(shape, context) {
 			context.fillStyle = '#EE3333';
 			context.beginPath();
 		   	var circle = shape;
-			var pos = circle.m_position;
+			var pos = circle.m_localPosition;
 			var r = circle.m_radius;
 			var segments = 50.0;
 			var theta = 0.0;
@@ -25,45 +24,53 @@ $(function() {
 				theta += dtheta;
 			}
 			context.lineTo(pos.x + r, pos.y);
-		
-			// draw radius
-			context.moveTo(pos.x, pos.y);
-			var ax = circle.m_R.col1;
-			var pos2 = new b2Vec2(pos.x + r * ax.x, pos.y + r * ax.y);
-			context.lineTo(pos2.x, pos2.y);
-			context.fill();
 		};
-		
-		var drawPoly = function(shape, context) {
+				
+	    var drawPoly = function(shape, context) {
 			context.strokeStyle = '#EE3333';
 			context.beginPath();
 			var poly = shape;
-			var tV = b2Math.AddVV(poly.m_position, b2Math.b2MulMV(poly.m_R, poly.m_vertices[0]));
+			var tV = b2Math.AddVV(poly.m_localPosition, b2Math.b2MulMV(poly.m_R, poly.m_vertices[0]));
 			context.moveTo(tV.x, tV.y);
 			for (var i = 0; i < poly.m_vertexCount; i++) {
-				var v = b2Math.AddVV(poly.m_position, b2Math.b2MulMV(poly.m_R, poly.m_vertices[i]));
+				var v = b2Math.AddVV(poly.m_localPosition, b2Math.b2MulMV(poly.m_R, poly.m_vertices[i]));
 				context.lineTo(v.x, v.y);
 			}
 			context.lineTo(tV.x, tV.y);
 			context.stroke();
-		};		
-		
-		return {					
+		};	
+
+		return {	
+
 			drawBall: drawBall,
 			
 			drawPoly: drawPoly,
-			
-			drawWorld: function(world, context) {
+	
+			draw: function(world, context) {
 				for (var b = world.m_bodyList; b; b = b.m_next) {
-					for (var s = b.GetShapeList(); s != null; s = s.GetNext()) {
+					for (var s = b.GetShapeList(); s != null; s = s.GetNext()) {				        
 						if (s.m_type == b2Shape.e_circleShape) {
 							drawBall(s, context);
 						} else {
-							drawPoly(s, context);
+							//drawPoly(s, context);
 						}
 					}
 				}    
+			},
+
+			drawWorld: function(bodies, context) {
+			    for(var i = 0; i < bodies.length; i++){
+			        var body = bodies[i];
+			        var t = body.m_xf;
+			        context.strokeStyle = '#EE3333';
+			        context.translate(t.position.x, t.position.y);
+			        context.rotate(body.GetAngle());
+			        context.fillRect(-body.w, -body.h, body.w*2, body.h*2);
+			        context.rotate(-body.GetAngle());
+			        context.translate(-t.position.x, -t.position.y);
+			    }
 			}
+		  
 		};
 	}();
 });	
