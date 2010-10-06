e0ae585262a37621415509fa7009254827c33c30
jarod.long64 <jarod.long64@19108cbd-a829-df12-ab1d-8371e1d2e174>
2010-10-06 05:13:28 +0000

    Converted the Level type and game states to use Object.create.

--- trunk/src/main/web/scripts/states/main_menu_state.js
+++ trunk/src/main/web/scripts/states/main_menu_state.js
@@ -5,39 +5,48 @@
  */
 
 $(function () {
-
-    m3.states.MainMenuState = function () {};
-    
-    m3.states.MainMenuState.prototype.keyHandlers = {
-        ENTER: {
-            down: function() {
-                m3.game.state = new m3.states.PlayState();
+    m3.states.MainMenuState = function () {
+        return {
+            // Keyboard input handlers for the main menu state.
+            keyHandlers: {
+                ENTER: {
+                    down: function() {
+                        m3.game.state = m3.states.PlayState.create();
+                    }
+                }
+            },
+            
+            // Mouse input handlers for the main menu state.
+            mouseHandlers: {
+               down: function(event) {
+                   m3.game.state = m3.states.PlayState.create();
+               }
+            },
+            
+            // Main update function for the main menu state.
+            update: function() {
+                var context     = m3.game.context,
+                    half_width  = m3.game.width / 2,
+                    half_height = m3.game.height / 2;
+                
+                // Draw the background.
+                context.fillStyle = "rgb(200, 220, 250)";
+                context.fillRect(0, 0, m3.game.width, m3.game.height);
+                
+                // Draw the text.
+                context.fillStyle = "rgba(0, 0, 0, 0.8)";
+                context.font      = "bold 48px sans-serif";
+                context.textAlign = "center";
+                context.fillText("Manic Monkey Madness!!!", half_width, half_height);
+                
+                context.font = "bold 30px sans-serif";
+                context.fillText("PLAY", half_width, half_height + 80);
+            },
+            
+            // "Constructor".
+            create: function() {
+                return Object.create(this);
             }
-        }
-    };
-    
-    m3.states.MainMenuState.prototype.mouseHandlers = {
-       down: function(event) {
-           m3.game.state = new m3.states.PlayState();
-       }
-    };
-    
-    m3.states.MainMenuState.prototype.update = function () {
-        var context    = m3.game.context,
-            halfWidth  = m3.game.width / 2,
-            halfHeight = m3.game.height / 2;
-        
-        // Draw the background.
-        context.fillStyle = "rgb(200, 220, 250)";
-        context.fillRect(0, 0, m3.game.width, m3.game.height);
-        
-        // Draw the text.
-        context.fillStyle = "rgba(0, 0, 0, 0.8)";
-        context.font      = "bold 48px sans-serif";
-        context.textAlign = "center";
-        context.fillText("Manic Monkey Madness!!!", halfWidth, halfHeight);
-        
-        context.font = "bold 30px sans-serif";
-        context.fillText("PLAY", halfWidth, halfHeight + 80);
-    };
+        };
+    }();
 });
--- trunk/src/main/web/scripts/states/play_state.js
+++ trunk/src/main/web/scripts/states/play_state.js
@@ -7,162 +7,170 @@
 
 $(function() {
     m3.states.PlayState = function() {
-        this.level = new m3.types.Level();
-        
-        // The state determines what we're doing right now. It can be one of:
-        //   
-        //   - starting:      The game just loaded, and we're waiting a moment to actually
-        //                    start the game.
-        //   - waiting:       It's a player's turn, and we're waiting for them to launch
-        //                    a projectile.
-        //   - attacking:     The player has launched a projectile, and we're letting the
-        //                    physics system do its work.
-        //   - transitioning: After the attacking state is finished, we're transitioning
-        //                    back to the other player.
-        this.game_state = "starting";
-        
-        // This keeps track of how long we've been in the current state.
-        this.state_time = 0.0;
-        
-        // This determines whose turn it is. The player who starts is chosen randomly.
-        this.active_player = m3.math.randomInteger(0, 1);
-        
-        // This is a reference to the projectile most recently launched.
-        this.active_projectile = null;
-        
-        // If the second player is starting, we need to warp the camera to their side.
-        if (this.active_player === 1) {
-            m3.camera.warp(m3.config.level_width - m3.game.width, 0);
-        }
-        
-    };
-    
-    m3.states.PlayState.prototype.keyHandlers = {
-        ENTER: {
-            down: function() {
-                m3.camera.slideTo(m3.config.level_width - m3.game.width, 0, "smooth");
-            }
-        },
-        
-        S: {
-            down: function() {
-                m3.score.player_scores[1] += 5.0;
-            }
-        },
-        
-        W: {
-            down: function() {
-                m3.launcher.changeWeapon();
-            }
-        }
-    };
-    
-    m3.states.PlayState.prototype.mouseHandlers = {
-        down: function(event) {
-            var state = m3.game.state;
+        return {
+            // The state's level.
+            level: null,
             
-            if (state.game_state === "waiting") {
-                m3.launcher.prepareLaunch(event);
-            }
-        },
-        
-        up: function(event) {
-            var state = m3.game.state;
+            // The state determines what we're doing right now. It can be one of:
+            //   
+            //   - starting:      The game just loaded, and we're waiting a moment to actually
+            //                    start the game.
+            //   - waiting:       It's a player's turn, and we're waiting for them to launch
+            //                    a projectile.
+            //   - attacking:     The player has launched a projectile, and we're letting the
+            //                    physics system do its work.
+            //   - transitioning: After the attacking state is finished, we're transitioning
+            //                    back to the other player.
+            game_state: "starting",
             
-            if (state.game_state === "waiting") {
-                state.setState("attacking");
-                m3.launcher.launch(event);
-            }
-        },
-        
-        move: function(event) {
-            var state = m3.game.state;
+            // This keeps track of how long we've been in the current state.
+            state_time: 0.0,
             
-            if (state.game_state === "waiting") {
-                m3.launcher.aim(event);
-            }
-        }
-    };
-    
-    /**
-     * The setter for the state also resets the state timer.
-     */
-    m3.states.PlayState.prototype.setState = function(new_state) {
-        this.game_state = new_state;
-        this.state_time = 0.0;
-    };
-    
-    /**
-     * This is the update function for the starting state.
-     */
-    m3.states.PlayState.prototype.updateStarting = function() {
-        if (this.state_time >= 0.5) {
-            this.setState("waiting");
-        }
-    };
-    
-    /**
-     * This is the update function for the waiting state.
-     */
-    m3.states.PlayState.prototype.updateWaiting = function() {
-        // Nothing yet.
-    };
-    
-    /**
-     * This is the update function for the attacking state.
-     */
-    m3.states.PlayState.prototype.updateAttacking = function() {
-        // For now, after the player shoots their cannon, we switch to the other player after 5 seconds.
-        // Eventually we should instead have some way of detecting when the physics have "settled down",
-        // like angry birds does.
-        if (this.state_time >= 5.0) {
-            var camera_position = (this.active_player === 0) ? m3.config.level_width - m3.game.width : 0;
-            m3.camera.stopFollowing();
-            m3.camera.slideTo(camera_position, 0, "smooth");
-            this.active_player = (this.active_player + 1) % 2;
-            this.active_projectile.destroy();
-            this.active_projectile = null;
-            this.setState("transitioning");
-        }
-    };
-    
-    /**
-     * This is the update function for the transitioning state.
-     */
-    m3.states.PlayState.prototype.updateTransitioning = function() {
-        // We're done transitioning when the camera is done sliding.
-        if (!m3.camera.sliding) {
-            this.setState("waiting");
-        }
-    };
-    
-    m3.states.PlayState.prototype.update = function() {
-        this.state_time += m3.game.elapsed;
-        
-        // Update the state itself.
-        switch (this.game_state) {
-            case ("starting"):
-                this.updateStarting(); break;
-            case ("waiting"):
-                this.updateWaiting(); break;
-            case ("attacking"):
-                this.updateAttacking(); break;
-            case ("transitioning"):
-                this.updateTransitioning(); break;
-            default:
-                m3.util.log("Play state has entered an invalid state!");
-        }
-        
-        // Update modules.
-        m3.camera.update();
-        this.level.update();
-        m3.world.update();
-        m3.launcher.update();
-        
-        if (this.active_projectile) {
-            this.active_projectile.update();
-        }
-        
-        m3.ui.update();
-    };
+            // This determines whose turn it is. The player who starts is chosen randomly.
+            active_player: 0,
+            
+            // This is a reference to the projectile most recently launched.
+            active_projectile: null,
+            
+            // Keyboard input handlers for the play state.
+            keyHandlers: {
+                ENTER: {
+                    down: function() {
+                        m3.camera.slideTo(m3.config.level_width - m3.game.width, 0, "smooth");
+                    }
+                },
+                
+                S: {
+                    down: function() {
+                        m3.score.player_scores[1] += 5.0;
+                    }
+                },
+                
+                W: {
+                    down: function() {
+                        m3.launcher.changeWeapon();
+                    }
+                }
+            },
+            
+            // Mouse input handlers for the main menu state.
+            mouseHandlers: {
+                down: function(event) {
+                    var state = m3.game.state;
+                    
+                    if (state.game_state === "waiting") {
+                        m3.launcher.prepareLaunch(event);
+                    }
+                },
+                
+                up: function(event) {
+                    var state = m3.game.state;
+                    
+                    if (state.game_state === "waiting") {
+                        state.setState("attacking");
+                        m3.launcher.launch(event);
+                    }
+                },
+                
+                move: function(event) {
+                    var state = m3.game.state;
+                    
+                    if (state.game_state === "waiting") {
+                        m3.launcher.aim(event);
+                    }
+                }
+            },
+            
+            // The setter for the state also resets the state timer.
+            setState: function(new_state) {
+                this.game_state = new_state;
+                this.state_time = 0.0;
+            },
+            
+            // This is the update function for the starting state.
+            updateStarting: function() {
+                if (this.state_time >= 0.5) {
+                    this.setState("waiting");
+                }
+            },
+            
+            // This is the update function for the waiting state.
+            updateWaiting: function() {
+                // Nothing yet.
+            },
+            
+            // This is the update function for the attacking state.
+            updateAttacking: function() {
+                // For now, after the player shoots their cannon, we switch to the other player after 5 seconds.
+                // Eventually we should instead have some way of detecting when the physics have "settled down",
+                // like angry birds does.
+                if (this.state_time >= 5.0) {
+                    var camera_position = (this.active_player === 0) ? m3.config.level_width - m3.game.width : 0;
+                    m3.camera.stopFollowing();
+                    m3.camera.slideTo(camera_position, 0, "smooth");
+                    this.active_player = (this.active_player + 1) % 2;
+                    this.active_projectile.destroy();
+                    this.active_projectile = null;
+                    this.setState("transitioning");
+                }
+            },
+            
+            // This is the update function for the transitioning state.
+            updateTransitioning: function() {
+                // We're done transitioning when the camera is done sliding.
+                if (!m3.camera.sliding) {
+                    this.setState("waiting");
+                }
+            },
+            
+            // This is the main update function which mostly just calls other update functions.
+            update: function() {
+                this.state_time += m3.game.elapsed;
+                
+                // Update the state itself.
+                switch (this.game_state) {
+                    case ("starting"):
+                        this.updateStarting(); break;
+                    case ("waiting"):
+                        this.updateWaiting(); break;
+                    case ("attacking"):
+                        this.updateAttacking(); break;
+                    case ("transitioning"):
+                        this.updateTransitioning(); break;
+                    default:
+                        m3.util.log("Play state has entered an invalid state!");
+                }
+                
+                // Update modules.
+                m3.camera.update();
+                this.level.update();
+                m3.world.update();
+                m3.launcher.update();
+                
+                if (this.active_projectile) {
+                    this.active_projectile.update();
+                }
+                
+                m3.ui.update();
+            },
+            
+            // "Constructor".
+            create: function() {
+                var state = Object.create(this);
+                
+                state.level = m3.types.Level.create();
+                state.active_player = m3.math.randomInteger(0, 1);
+                
+                // If the second player is starting, we need to warp the camera to their side.
+                if (state.active_player === 1) {
+                    m3.camera.warp(m3.config.level_width - m3.game.width, 0);
+                }
+                else {
+                    m3.camera.warp(0, 0);
+                }
+                
+                return state;
+            },
+        };
+    }();
 });
