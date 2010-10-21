259c7a70a26ae2eb6cb62f291ba2c89bc291861a
Jarod Long <jlong22@me.com>
2010-10-21 07:22:57 +0000

    Fort pieces now change sprites based on their damage level. Currently only long wood pieces have graphics to support this however.

--- src/main/web/scripts/types/projectile.js
+++ src/main/web/scripts/types/projectile.js
@@ -19,10 +19,10 @@ $(function() {
             }
         
         };
-            
+        
         var details = {
-            small:  { density: 2.0, restitution: 0.1, friction: 1.0 },
-            single: { density: 1.5, restitution: 0, friction: 0.85 }
+            small:  { density: 2.0, restitution: 0.1, friction: 1.25 },
+            single: { density: 1.5, restitution: 0,   friction: 1.0 }
         };
         
         return {
@@ -35,7 +35,13 @@ $(function() {
                         m3.util.log('fort piece damage: ' + other.damage.toFixed(2));
                     }
                     
-                    if (other.damage > other.destroyThreshold) {
+                    if (other.damage >= other.destroyThreshold / 3 && other.damage < other.destroyThreshold * 2 / 3 && other.sprites.damaged) {
+                        other.sprite = other.sprites.damaged;
+                    }
+                    else if (other.damage >= other.destroyThreshold * 2 / 3 && other.damage < other.destroyThreshold && other.sprites.destroyed) {
+                        other.sprite = other.sprites.destroyed;
+                    }
+                    else if (other.damage > other.destroyThreshold) {
                         other.alive = false;
                         m3.util.log('fort piece destroyed');
                         m3.score.playerDestroyed(other);
@@ -74,7 +80,7 @@ $(function() {
                 if (impulse_x !== undefined && impulse_y !== undefined) {
                     p.body.ApplyImpulse(new b2Vec2(impulse_x, impulse_y), new b2Vec2(p.x_in_meters, p.y_in_meters));
                     
-                    var torque = (impulse_x < 0) ? -400.0 : 400.0;
+                    var torque = (impulse_x < 0) ? -800.0 : 800.0;
                     p.body.ApplyTorque(torque);
                 }
                 
