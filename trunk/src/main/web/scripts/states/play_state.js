01580e5b1d5a977b304cc4938cb25a2a6dcd0680
anthonykhayat1 <anthonykhayat1@19108cbd-a829-df12-ab1d-8371e1d2e174>
2010-11-09 18:44:23 +0000

    Made the arrow bigger and it also have the turn change faster

--- trunk/src/main/web/scripts/states/play_state.js
+++ trunk/src/main/web/scripts/states/play_state.js
@@ -163,16 +163,16 @@ $(function() {
             for (var i = 0, j = this.active_projectile.length; i < j; i+=1) {
                 var position = this.active_projectile[i].body.GetPosition();
                     x = position.x * m3.config.scaling_factor;
-                    y = position.y * m3.config.scaling_factor;
-                
+            	    y = position.y * m3.config.scaling_factor;
+                   
                 settled = false;
                 y < 0 ? m3.ui.marker.mark(x) : m3.ui.marker.clearMark(); 
             }
             
             // This ensures the world is settled for half a second before transitioning.
-            if (m3.world.allSettled(0.5)) {
-                if (this.old_time === null) this.old_time = this.state_time;
-                else if (this.state_time > this.old_time + 0.5) settled = true;
+            if (m3.world.allSettled(1.0)) {
+                if(this.old_time === null) this.old_time = this.state_time;
+                else if(this.state_time > this.old_time + 0.25) settled = true;
             } else {
                 this.old_time = null;
             }
