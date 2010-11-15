6ca4e142c219d5da7333ea4e00c08e8a997c026f
mkronk3 <mkronk3@19108cbd-a829-df12-ab1d-8371e1d2e174>
2010-11-15 06:50:53 +0000

    integrated new background images, create level data structure for demo level, cleaned up image file organization

--- trunk/src/main/web/scripts/modules/buttons.js
+++ trunk/src/main/web/scripts/modules/buttons.js
@@ -1,13 +1,27 @@
-function toggleHelp() {
-	var help = $("#top").get(0);
-	var middle = $("#middle").get(0);
+/**
+ * buttons.js
+ * 
+ * This module holds functions for any canvas external buttons in the web page.
+ */
+
+$(function() {
 	
-	if (help.style.display === "block") {
-		help.style.display = "none";
-		middle.style.display = "none";
-	}
-	else {
-		help.style.display = "block";
-		middle.style.display = "block";
-	}
-}
+	m3.buttons = function() {
+		return {
+			toggleHelp: function() {
+				var help = $("#top").get(0);
+				var middle = $("#middle").get(0);
+				
+				if (help.style.display === "block") {
+					help.style.display = "none";
+					middle.style.display = "none";
+				}
+				else {
+					help.style.display = "block";
+					middle.style.display = "block";
+				}
+			}	
+			
+		};
+	}();
+});
