42ab1e9d168ca5ba5ecf3f7313f985d98d2b73cf
Jarod Long <jlong22@me.com>
2010-11-30 18:39:57 +0000

    Fixed some level editor bugs.

--- src/main/web/scripts/modules/util.js
+++ src/main/web/scripts/modules/util.js
@@ -14,33 +14,45 @@ $(function() {
         
         return {
             log: function(text) {
-        	    if (log_enabled) {
+                if (log_enabled) {
                     $("#console_items").prepend("<p>" + text + "</p>");
                     log_count++;
-                
+                    
                     if (log_count > max_log_count) {
                         $("#console_items").children("p").first().remove();
                     }
-        	    }
+                }
             },
             
             toggleLog: function() {
-            	log_enabled = !log_enabled;
-            	$('#console').toggle();
+                log_enabled = !log_enabled;
+                $('#console').toggle();
             },
             
             /*
              * Converts an array of vertices in pixels to an array of box2d b2Vec vertices in meters
              */
-            pixelsToMeters : function(vertices) {            	
-            	var scale = m3.config.scaling_factor,
-            	    verticesInMeters = [];
-            	
-            	$.each(vertices, function(i) {
-            		verticesInMeters.push(new b2Vec2(this[0] / scale, this[1] / scale));
-            	});
-            	
-            	return verticesInMeters;
+            pixelsToMeters: function(vertices) {
+                var scale = m3.config.scaling_factor,
+                    verticesInMeters = [];
+                
+                $.each(vertices, function(i) {
+                    verticesInMeters.push(new b2Vec2(this[0] / scale, this[1] / scale));
+                });
+                
+                return verticesInMeters;
+            },
+            
+            // Clamps a number within a range.
+            clamp: function(n, min, max) {
+                if (n < min) {
+                    return min;
+                }
+                else if (n > max) {
+                    return max;
+                }
+                
+                return n;
             }
         };
     }();
