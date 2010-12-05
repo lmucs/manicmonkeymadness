46cf35fc707c47a741a9f0e41514d25bcd5bfcbb
ray.toal@gmail.com <ray.toal@gmail.com@19108cbd-a829-df12-ab1d-8371e1d2e174>
2010-12-05 06:53:47 +0000

    Add some missing commas and semicolons.  Some cleanup (formatting).

--- trunk/src/main/web/scripts/types/fort_piece.js
+++ trunk/src/main/web/scripts/types/fort_piece.js
@@ -1,22 +1,21 @@
 /**
  * fort_piece.js
- * 
+ *
  * This object represents a chunk of a fort.
- * 
  */
 
-$(function() {
-    m3.types.FortPiece = function() {
+$(function () {
+    m3.types.FortPiece = function () {
         var FortPiece = Object.create(m3.types.PhysicsObject);
         FortPiece.FortPiece = FortPiece;
-        
+
         // Private members.
         var Sprite = m3.types.Sprite,
             Vector = m3.types.Vector,
-            b2Vec2 = Box2D.Common.Math.b2Vec2
+            b2Vec2 = Box2D.Common.Math.b2Vec2,
             assets = m3.assets.sprites.fort_pieces;
 
-        
+
         var pieces = {
             box: {
                 long: {
@@ -36,40 +35,40 @@ $(function() {
                     rock: { s: assets.box_wide_rock, h: 40, w: 20, cost: 16 }
                 }
             },
-            
+
             triangle: {
-            	small: {
-            		wood: { s: assets.triangle_wood, h: 18, w: 36, cost: 5, 
-            			    vertices: [[0,-12], [18, 6], [-18,6]], spriteOffset: Vector.create(18,12)},
-            		rock: { s: assets.triangle_rock, h: 18, w: 37, cost: 10, 
-            			    vertices: [[0,-12], [19, 6], [-19,5]], spriteOffset: Vector.create(19,12) }
-            	}
+                small: {
+                    wood: { s: assets.triangle_wood, h: 18, w: 36, cost: 5,
+                            vertices: [[0,-12], [18, 6], [-18,6]], spriteOffset: Vector.create(18,12)},
+                    rock: { s: assets.triangle_rock, h: 18, w: 37, cost: 10,
+                            vertices: [[0,-12], [19, 6], [-19,5]], spriteOffset: Vector.create(19,12) }
+                }
             }
         };
-        
+
         var materials = {
             wood: { density: 3.0,  restitution: 0.25, friction: 0.8, minImpactVelocity: 0.5,  destroyThreshold: 4.0 },
             rock: { density: 13.0, restitution: 0.5,  friction: 0.9, minImpactVelocity: 0.65, destroyThreshold: 8.0 }
         };
-        
+
         // Collision callback.
-        FortPiece.contact = function(other, velocity) {
+        FortPiece.contact = function (other, velocity) {
 
             if (m3.launcher.currentLauncher().pType === "watermelon" && other.type === "projectile") {
-            	other.type = "broken";
-            	other.body.SetLinearVelocity(new b2Vec2(0,0));
-            	other.sprite.play("explode");
-            	
-            	setTimeout(function(projectile){
-            		return function () {
-            			projectile.type = "done";
-            			m3.world.explode(new b2Vec2(projectile.x, projectile.y));
-            			projectile.alive = false;
-            		};
-            	}(other), 2000);
+                other.type = "broken";
+                other.body.SetLinearVelocity(new b2Vec2(0,0));
+                other.sprite.play("explode");
+
+                setTimeout(function (projectile) {
+                    return function () {
+                        projectile.type = "done";
+                        m3.world.explode(new b2Vec2(projectile.x, projectile.y));
+                        projectile.alive = false;
+                    };
+                }(other), 2000);
                 return;
             }
-        	
+
             if (other.type === 'fort_piece') {
                 if (velocity > this.minImpactVelocity) {
 //                    m3.util.log('fort piece hit fort piece at: ' + velocity.toFixed(2) + 'm/s');
@@ -77,7 +76,7 @@ $(function() {
 //                    m3.util.log('fort piece damage: ' + this.damage.toFixed(2));
 //                    m3.util.log('fort piece damage: ' + other.damage.toFixed(2));
                 }
-                
+
                 if (other.damage >= other.destroyThreshold / 3 && other.damage < other.destroyThreshold * 2 / 3 && other.sprites.damaged) {
                     other.sprite = other.sprites.damaged;
                 }
@@ -97,7 +96,7 @@ $(function() {
 //                    m3.util.log('fort piece damage: ' + this.damage.toFixed(2));
 //                    m3.util.log('enemy damage: ' + other.damage.toFixed(2));
                 }
-                
+
                 if (other.damage > other.destroyThreshold) {
                     other.alive = false;
 //                    m3.util.log('enemy destroyed');
@@ -105,13 +104,13 @@ $(function() {
                 }
             }
         };
-        
+
         // Override update so we can draw a circle on the piece if we're in the edit level state.
-        FortPiece.update = function() {
+        FortPiece.update = function () {
             this.PhysicsObject.update.call(this);
-            
+
             var context = m3.game.context;
-            
+
             if (m3.game.state.EditLevelState) {
                 context.fillStyle = (this.out_of_bounds) ? "rgba(255, 20, 10, 0.6)" : "rgba(100, 180, 255, 0.6)";
                 context.beginPath();
@@ -120,7 +119,7 @@ $(function() {
                 context.closePath();
             }
         };
-        
+
         // Constructor.
         FortPiece.create = function(fort, shape, size, material, x, y, angle, container, fixed) {
             var f     = m3.types.PhysicsObject.create(x, y, container, this),
@@ -130,8 +129,8 @@ $(function() {
                 piece = t.vertices ? m3.world.createPoly(x / scale, y / scale, m3.util.pixelsToMeters(t.vertices), angle,
                                            fixed, m.density, m.restitution, m.friction)
                                    : m3.world.createBox(x / scale, y / scale, t.w / scale, t.h / scale, angle,
-                                           fixed, m.density, m.restitution, m.friction);                      
-            
+                                           fixed, m.density, m.restitution, m.friction);
+
             piece.body.SetUserData(f);
             f.sprites = {};
             f.sprites.normal    = Sprite.create(t.s.normal, t.h, t.w);
@@ -139,9 +138,9 @@ $(function() {
             f.sprites.destroyed = t.s.destroyed ? Sprite.create(t.s.damaged, t.h, t.w) : null;
             f.destroyThreshold  = m.destroyThreshold;
             f.minImpactVelocity = m.minImpactVelocity;
-            
+
             if (t.spriteOffset) f.spriteOffset = t.spriteOffset;
-            
+
             f.piece_shape    = shape;
             f.piece_size     = size;
             f.piece_material = material;
@@ -154,10 +153,10 @@ $(function() {
             f.alive          = true;
             f.damage         = 0;
             f.out_of_bounds  = false;
-            
+
             return f;
         };
-        
+
         return FortPiece;
     }();
 });
