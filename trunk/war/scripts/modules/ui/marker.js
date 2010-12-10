961dc1a91e4f13424259575e17e68fd2c41e0c23
mkronk3 <mkronk3@19108cbd-a829-df12-ab1d-8371e1d2e174>
2010-12-10 07:32:06 +0000

    fixed tracking arrow to draw on top of rest of the UI, and white mark on corner of fort piece

--- trunk/war/scripts/modules/ui/marker.js
+++ trunk/war/scripts/modules/ui/marker.js
@@ -8,8 +8,8 @@
 $(function() {
     m3.ui.marker = function() {
    
-    	var context = m3.game.context;
-            projectile = [];
+    	var context = m3.game.context,
+            projectile = [],
             arrow = m3.assets.icons.arrow;
     	    
     	return {
@@ -19,7 +19,7 @@ $(function() {
     		},
     		
     		update: function() {
-    			//draws an arrow for each active projectile
+    			// Draws an arrow for each active projectile
     			for (var i = 0, n = projectile.length; i < n; i+=1) {
     				position = projectile[i].body.GetPosition();
     			    if (position.y * m3.config.scaling_factor < 0) {
