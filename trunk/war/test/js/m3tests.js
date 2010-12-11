87c9581217a79a891a3b5a079c0d3d11999006d5
ray.toal@gmail.com <ray.toal@gmail.com@19108cbd-a829-df12-ab1d-8371e1d2e174>
2010-12-11 09:37:04 +0000

    Add tests for the game choices module

--- trunk/war/test/js/m3tests.js
+++ trunk/war/test/js/m3tests.js
@@ -1,8 +1,17 @@
 $(function() {
-module("Module launcher");
+    module("Game Choices");
 
-test("first test within module", function() {
-  ok( true, "all pass" );
-});
+    test("Game choices initialization", function () {
+        var choices = m3.game_choices;
+        same(choices.max_shots, 0, "max shots initialized properly");
+        same(choices.game_mode, null, "game mode initialized properly");
+    });
 
+    test("Game choices manipulation", function () {
+        var choices = m3.game_choices;
+        choices.setGameLength(20);
+        choices.setGameMode("last monkey standing");
+        same(choices.max_shots, 20, "max shots setter okay");
+        same(choices.game_mode, "last monkey standing", "game mode setter okay");
+    });
 });
