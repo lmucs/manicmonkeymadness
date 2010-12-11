5af5950f34d1f4c3738b4646d5b6f3aad0f1591d
Ray Toal <rtoal@lmu.edu>
2010-12-11 09:39:06 +0000

    Format more reasonably.  Add better comments.

--- war/scripts/modules/game_choices.js
+++ war/scripts/modules/game_choices.js
@@ -1,25 +1,27 @@
 /**
  * game_choices.js
- * 
- * This module updates the total number of turns the players have selected to play.
- * 
+ *
+ * The m3.game_choices module holds options for the current game, such as the maximum number
+ * of shots allowed, and the name of the game mode.
+ *
+ * TODO: Needs refactoring. Why are there setters if the properties are both readable and
+ * writable?
  */
-
-$(function() {
-    m3.game_choices = function() {
+$(function () {
+    m3.game_choices = function () {
         var game_choices = {};
-    	
-    	game_choices.max_shots = 0;
-    	game_choices.game_mode = null;
 
-        game_choices.setGameLength = function(shots) {
+        game_choices.max_shots = 0;
+        game_choices.game_mode = null;
+
+        game_choices.setGameLength = function (shots) {
             this.max_shots = shots;
         };
-        
-        game_choices.setGameMode = function(mode) {
-        	this.game_mode = mode;
+
+        game_choices.setGameMode = function (mode) {
+            this.game_mode = mode;
         };
-        
+
         return game_choices;
     }();
 });
