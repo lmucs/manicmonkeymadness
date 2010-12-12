93afe919e20c6e396f14a48fcac58aa23f81c7f4
ray.toal@gmail.com <ray.toal@gmail.com@19108cbd-a829-df12-ab1d-8371e1d2e174>
2010-12-12 01:11:24 +0000

    More tests.

--- trunk/war/test/js/m3tests.js
+++ trunk/war/test/js/m3tests.js
@@ -14,4 +14,18 @@ $(function() {
         same(choices.max_shots, 20, "max shots setter okay");
         same(choices.game_mode, "last monkey standing", "game mode setter okay");
     });
+
+    module("Fort Choices");
+
+    test("Premade Forts", function () {
+        var choices = m3.fort_choices,
+            premade = m3.fort_choices.premade;
+        ok(premade.length >= 2, "Premade forts exist");
+        same(premade[0].id.toLowerCase(), "original", "First fort is original");
+        same(premade[1].id.toLowerCase(), "alternate", "Second fort is alternate");
+        for (var i = 0; i < premade.length; i += 1) {
+            ok(premade[i].fort.pieces.length > 1, "Fort " + i + " has pieces");
+            ok(premade[i].fort.enemies.length > 1, "Fort " + i + " has enemies");
+        }
+    });
 });
