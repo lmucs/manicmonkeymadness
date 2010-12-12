b1f1b54521a1eea7042e36032e78248003d77fee
Ray Toal <rtoal@lmu.edu>
2010-12-12 01:42:00 +0000

    Refactor game choices to have a private state

--- war/test/js/m3tests.js
+++ war/test/js/m3tests.js
@@ -3,16 +3,16 @@ $(function() {
 
     test("Game choices initialization", function () {
         var choices = m3.game_choices;
-        same(choices.max_shots, 0, "max shots initialized properly");
-        same(choices.game_mode, null, "game mode initialized properly");
+        same(choices.getGameLength(), 0, "max shots initialized properly");
+        same(choices.getGameMode(), null, "game mode initialized properly");
     });
 
     test("Game choices manipulation", function () {
         var choices = m3.game_choices;
         choices.setGameLength(20);
         choices.setGameMode("last monkey standing");
-        same(choices.max_shots, 20, "max shots setter okay");
-        same(choices.game_mode, "last monkey standing", "game mode setter okay");
+        same(choices.getGameLength(), 20, "max shots setter okay");
+        same(choices.getGameMode(), "last monkey standing", "game mode setter okay");
     });
 
     module("Fort Choices");
