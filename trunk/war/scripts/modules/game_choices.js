e18b400c5144822dd58a1dc98e2b0f276c266318
ray.toal@gmail.com <ray.toal@gmail.com@19108cbd-a829-df12-ab1d-8371e1d2e174>
2010-12-12 01:42:00 +0000

    Refactor game choices to have a private state

--- trunk/war/scripts/modules/game_choices.js
+++ trunk/war/scripts/modules/game_choices.js
@@ -2,26 +2,30 @@
  * game_choices.js
  *
  * The m3.game_choices module holds options for the current game, such as the maximum number
- * of shots allowed, and the name of the game mode.
- *
- * TODO: Needs refactoring. Why are there setters if the properties are both readable and
- * writable?
+ * of shots allowed, and the name of the game mode.  For fun, the module is implemented as
+ * a single object with no exposed properties, only old-style getters and setters.
  */
 $(function () {
     m3.game_choices = function () {
-        var game_choices = {};
+        var maxShots = 0,
+            gameMode = null;
 
-        game_choices.max_shots = 0;
-        game_choices.game_mode = null;
+        return {
+            getGameLength: function () {
+                return maxShots;
+            },
 
-        game_choices.setGameLength = function (shots) {
-            this.max_shots = shots;
+            setGameLength: function (shots) {
+                maxShots = shots;
+            },
+
+            getGameMode: function () {
+                return gameMode;
+            },
+
+            setGameMode: function (mode) {
+                gameMode = mode;
+            }
         };
-
-        game_choices.setGameMode = function (mode) {
-            this.game_mode = mode;
-        };
-
-        return game_choices;
     }();
 });
