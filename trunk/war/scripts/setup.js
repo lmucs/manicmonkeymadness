4e6f37ae822681f3e4e6d4c107715723683b99fe
ray.toal@gmail.com <ray.toal@gmail.com@19108cbd-a829-df12-ab1d-8371e1d2e174>
2010-12-12 06:19:39 +0000

    Some formatting, some cleanup.

--- trunk/war/scripts/setup.js
+++ trunk/war/scripts/setup.js
@@ -1,16 +1,18 @@
 /**
  * setup.js
- * 
+ *
  * This file contains all the architecture needed to initialize the game. It declares
  * the global m3 object that contains all other game modules.
- * 
+ *
+ * Include this script before any other.
  */
 
 // The global game object.
 var m3 = {
+
     // A "container" object to hold the constructors for our objects.
     types: {},
-    
+
     // Holds the constructors for our state objects.
     states: {}
 };
@@ -18,16 +20,8 @@ var m3 = {
 // Implement Object.create if the browser didn't.
 if (typeof Object.create !== "function") {
     Object.create = function(o) {
-        var f = function() {};
-        f.prototype = o;
-        return new f();
+        var F = function() {};
+        F.prototype = o;
+        return new F();
     };
 }
-
-// Breaks Box2D, Dr. Toal was right this was trouble
-//// Give arrays a remove function.
-//Array.prototype.remove = function(from, to) {
-//    var rest = this.slice((to || from) + 1 || this.length);
-//    this.length = from < 0 ? this.length + from : from;
-//    return this.push.apply(this, rest);
-//};
