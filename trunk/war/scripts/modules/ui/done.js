943b29ac5eb4b9b6c9696af645b3bc03bf6450b5
mkronk3 <mkronk3@19108cbd-a829-df12-ab1d-8371e1d2e174>
2010-12-16 09:32:42 +0000

    added rounded corners to dialogs, and decreased treasure size

--- trunk/war/scripts/modules/ui/done.js
+++ trunk/war/scripts/modules/ui/done.js
@@ -11,23 +11,24 @@ $(function() {
             ui          = m3.ui,
             camera      = m3.camera.position,
             half_width  = m3.game.width / 2,
-            half_height = m3.game.height / 2;
+            half_height = m3.game.height / 2,
+            treasure    = m3.assets.icons.treasure;
         
         return {
             update: function() {
                 // Say who won.
-                context.drawImage(m3.assets.icons.treasure, camera.x, camera.y);
+                context.drawImage(treasure, camera.x + half_width - (treasure.width / 2), m3.game.height - (treasure.height * 0.3), treasure.width * 0.3, treasure.height * 0.3);
                 context.fillStyle   = "rgba(250, 255, 245, 0.95)";
                 context.font        = "60px Tahoma, Geneva, sans-serif";
                 context.textAlign   = "center";
                 context.lineWidth   = 3;
                 context.strokeStyle = "rgba(20, 15, 0, 0.75)";
-                ui.drawStrokedText("Player " + (m3.game.state.winner + 1) + " wins!", camera.x + half_width, camera.y + half_height - 40);
+                ui.drawStrokedText("Player " + (m3.game.state.winner + 1) + " Wins!", camera.x + half_width, camera.y + half_height - 40);
                 
                 // Let them continue or quit.
                 context.font      = "30px Tahoma, Geneva, sans-serif";
                 context.lineWidth = 3;
-                ui.drawStrokedText("Press N for a new game, or Q to quit", camera.x + half_width, camera.y + half_height + 40);
+                ui.drawStrokedText("Press N for a New Game, or Q to Quit", camera.x + half_width, camera.y + half_height + 40);
             }
         };
     }();
