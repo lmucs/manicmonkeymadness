89c12e3acab8b477da0bf98fbd16adb9fb1c421c
anthonykhayat1 <anthonykhayat1@19108cbd-a829-df12-ab1d-8371e1d2e174>
2010-12-16 10:57:30 +0000

    Now flashes a message when the turn changes.  Also changed the duration of how long the new weapon message is on the screen

--- trunk/war/scripts/modules/ui/turn.js
+++ trunk/war/scripts/modules/ui/turn.js
@@ -7,18 +7,22 @@
 
 $(function() {
     m3.ui.turn = function() {
-        var context = m3.game.context,
-            ui      = m3.ui,
-            camera  = m3.camera.position,
-            width   = m3.game.width;
+        var context     = m3.game.context,
+            ui          = m3.ui,
+            camera      = m3.camera.position,
+            game_width  = m3.game.width,
+            game_height = m3.game.height;
+        
         
         return {
+        	changingTurn: false,
+        	
             update: function() {
                 var active_player   = m3.game.state.active_player,
                     circle_position = m3.types.Vector.create(0, 0);
                 
                 if (active_player === 1) {
-                    circle_position.x = m3.game.width;
+                    circle_position.x = game_width;
                 }
                 
                 context.fillStyle = "rgba(250, 250, 200, 0.95)";
@@ -26,6 +30,15 @@ $(function() {
                 context.arc(camera.x + circle_position.x, camera.y + circle_position.y, 75.0, 0.0, Math.PI * 2, false);
                 context.fill();
                 context.closePath();
+
+//            	console.log(this.changingTurn);
+                if(this.changingTurn) {
+                    context.fillStyle   = "rgba(44, 200, 8, 0.95)";
+                    context.strokeStyle = "rgba(0, 25, 0, 0.75)";
+                    context.font        = "40px Tahoma, Geneva, sans-serif";
+                    context.textAlign   = "center";
+                    ui.drawStrokedText("Player " + (active_player + 1) + "'s turn!", camera.x + game_width / 2, camera.y + game_height / 3);
+                }
             }
         };
     }();
